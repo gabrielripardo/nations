76cd9b80d743c4809be01009108cefd2c75eee7f
gabrielripardo <gts.senna@gmail.com>
2021-07-19 20:59:36 -0300

    new route api that get big coutries list

--- src/componentes/api.js
+++ src/componentes/api.js
@@ -1,23 +1,53 @@
-import React,{useState, useEffect} from 'react'
+import React,{useState, useEffect, props} from 'react'
 import axios from 'axios'
 
 export default function ListaCarros(){
-    const [carros, setCarros] = useState([])
+    const [countries, setcountries] = useState([])
 
     useEffect(() => {
-            axios.get('https://cfbcursosapireactexemplo1.brcampos.repl.co')
+            axios.get('https://api.worldbank.org/v2/country/?page=1&per_page=50&format=json')
             .then(res=>{
-                const dadosCarros = res.data
-                setCarros(dadosCarros)
+                const dadoscountries = res.data
+                setcountries(dadoscountries[1])                
             })    
         }
     )
 
+    // console.log(countries)
     return(
-        <div>
-            {carros.map(
-                carro => <div key={carro.id} >{carro.id} - {carro.marca} - {carro.modelo}</div>
-            )}
+        <div>            
+            <table>
+                    <thead>
+                        <tr>
+                            <th>id</th>
+                            <th>Nome</th>
+                            <th>Região</th>
+                            <th>Nível de Renda</th>
+                            <th>Capital</th>
+                            <th>Longitude</th>
+                            <th>Latitude</th>
+                        </tr>
+                    </thead>                        
+                    <tbody>
+                    {countries.map(
+                        country => 
+                        <tr>
+                            {/* <div key={country.id} >{country.name} - {country.region.value}</div>     */}
+
+                            <td>{country.id}</td>
+                            <td>{country.name}</td>
+                            <td>{country.region.value}</td>
+                            <td>{country.incomeLevel.value}</td>
+                            <td>{country.capitalCity}</td>
+                            <td>{country.longitude}</td>
+                            <td>{country.latitude}</td>
+                        </tr>            
+                    )}
+                       
+                    </tbody>
+                </table>
         </div>
     )
 }
+
+// Api Reference: https://www.programmableweb.com/api/world-bank-country-rest-api-v2
